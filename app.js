20351dd346e6ad2338d5489068a57a3fbe3a94a5
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-13 21:52:52 +0600

    add an nested object

--- app.js
+++ app.js
@@ -41,4 +41,38 @@ const sum = ({name, age}) => {
 sum(animal); */
 
 
+const dreamGirl = [
+  {
+    dream1: {
+      name: "bbu",
+      height: "5.4",
+      family: [{ father: "rock", mother: "shila", sister: "chinki" }],
+      age: undefined,
+      contactInfo: [
+        {
+          facebook: {
+            link: "https://www.facebook.com/",
+            followers: "12545",
+            status: "single",
+            friendsList: [
+              { name: "rofik" },
+              { name: "jobbar" },
+              { name: "salam" },
+              { name: "borkot" },
+              undefined,
+            ],
+          },
+        },
+        { instagram: "https://www.instagram.com/" },
+        { twitter: "https://twitter.com/" },
+        { github: "https://github.com/" },
+        { phone: ["01254823212", "02152457"] },
+      ],
+    },
+  },
+];
+
+console.log(dreamGirl);
+
+
 
