24ccc6868cc3823f463e0b79054690255f3bde19
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-13 23:13:40 +0600

    ternary operator

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
-const data = () => {
+/* const data = () => {
     fetch('https://fakestoreapi.com/products')
         .then(res => res.json())
         .then(data => console.log(data));
 }
 data();
+ */
+
+// ternary operator
+const name = "kamrul";
+console.log(name ? "true return" : "false return");
