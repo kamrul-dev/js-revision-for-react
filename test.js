33980ec11b06bb604170ae8abfd1d0134b785213
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-14 19:45:41 +0600

    let , const

--- test.js
+++ test.js
@@ -9,3 +9,28 @@ data();
 // ternary operator
 const name = "kamrul";
 console.log(name ? "true return" : "false return");
+
+// let ,const 
+
+
+//let
+// ========
+// redeclare is not possible
+// reassign is possible
+// has block scope
+
+let a = 5;
+a = 6;
+
+console.loga(a); //output 6
+
+
+
+// const 
+// redeclare is not possible
+// reasssign is not possible
+// has block scope
+
+const x = 100;
+const x = 200; // redeclare is not possible in const
+x = 100 // resasssng not possible in const
